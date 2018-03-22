dc59110aad8be153388e8e470de6341b17ab0ef8
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-03-22 17:17:41 +0100

    remove hbrs

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 
 const app = express()
 const bodyParser = require('body-parser')
 
 // const routes = require('./routes')
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
