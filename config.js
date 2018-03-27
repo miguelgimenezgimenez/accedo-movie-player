5baaaf6f821dfb18667e17c3cd6cc3ffb0ff94fc
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-03-27 17:59:34 +0200

    remove useless elements from config

--- config.js
+++ config.js
@@ -3,14 +3,12 @@ const getConfig = () => {
   switch (process.env.NODE_ENV) {
     case 'development':
       return {
-        apiUrl: 'http://localhost:3000',
-        frontUrl: 'http://localhost:3000'
+        apiUrl: 'http://localhost:3000'
 
       }
     case 'production':
       return {
-        apiUrl: 'http://46.101.91.79',
-        frontUrl: 'http://miguelgimenez.tech'
+        apiUrl: 'http://46.101.91.79'
       }
     default:
       return {}
