d1bdf82d644c5c3e6a2f135017852d330b4eba89
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2018-03-27 18:04:48 +0200

    fix package

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { AppContainer } from 'react-hot-loader' // AppContainer is a necessary wrapper component for HMR
 
 import Layout from './components/layout'
 import routes from './routes'
@@ -12,24 +11,15 @@ const store = createStore({})
 
 const render = () => {
   ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Layout>
-            {routes}
-          </Layout>
-        </BrowserRouter>
-      </Provider>
-    </AppContainer>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          {routes}
+        </Layout>
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('app')
   )
 }
 
 render()
-
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/layout.js', () => {
-    render()
-  })
-}
